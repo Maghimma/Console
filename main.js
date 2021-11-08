2d9ae92fc789cb23bbed96a7d688f6c9eee49c8f
Maghimma <85837242+Maghimma@users.noreply.github.com>
2021-11-08 16:35:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("My-first-console-project");
+
+var first_name = "Maghimma.";
+var last_name = "S.O";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 15;
+
+var add = a + b;
+console.log(add)
+
+var sub = a - b;
+console.log(sub)
+
+var multiply = a * b;
+console.log(multiply)
+
+var divide = a / b;
+console.log(divide)
